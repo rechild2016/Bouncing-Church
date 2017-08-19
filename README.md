94b2ca4246f8550f851c71665a8d549d37ba6c7c
Rechild <1102530068@qq.com>
2017-08-19 21:37:19 +0800

    creat the Canvas

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 InitProgramme  hello world
 
 be in studying``````
+
+Now,i can creat a Canvas and show a picture.
+after that ,i can also move the pic.
--- main.py
+++ main.py
@@ -4,31 +4,23 @@
 import tkinter as tk
 import tkinter.messagebox as messagebox
 
-windows = tk.Tk()
-windows.title('TEST')
-windows.geometry('350x450')
+window = tk.Tk()
+window.title('TEST')
+window.geometry('500x500')
 
-e = tk.Entry(windows,show='*')
-e.pack()
+canvas = tk.Canvas(window,bg='green',height=450,width=500)
+img_file = tk.PhotoImage(file = 'images.png')
+image = canvas.create_image(0,0,anchor='nw',image=img_file)
+line = canvas.create_line(50,50,200,200,width= 3)
+oval = canvas.create_oval(200,200,300,300,fill='red')
+canvas.pack()
 
-def insert_point():
-    var = e.get()
-    t.insert('insert',var)
+def moveit():
+    canvas.move(image,5,5)
 
-def insert_end():
-    var = e.get()
-    t.insert('end',var)
+b = tk.Button(window,text='move',command=moveit)
+b.pack()
 
-button1 = tk.Button(windows,text = 'insert point',bg='green',width = 15,
-                    height = 2,command=insert_point)
-button1.pack()
 
-button2 = tk.Button(windows,text = 'insert end',bg='yellow',width = 15,
-                    height = 2,command=insert_end)
-button2.pack()
-
-t=tk.Text(windows,height=8)
-t.pack()
-
-windows.mainloop()
+window.mainloop()
 
