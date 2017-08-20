e5414d06d33e1e7c938f77d6426c73fff4066a47
Rechild <1102530068@qq.com>
2017-08-20 19:45:11 +0800

    Add Timer task

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Now,i can creat a Canvas and show a picture.
 after that ,i can also move the pic.
 
 Add Menubar, including restore and exit.
+
+Add Timer and move a red circle in the track.It's becoming a game!
--- main.py
+++ main.py
@@ -1,16 +1,53 @@
 
 #!/usr/bin/env python     
 
+import math
+import time
 import tkinter as tk
 import tkinter.messagebox as messagebox
+import threading
 
 window = tk.Tk()
 window.title('TEST')
-window.geometry('500x500')
+window.geometry('800x600')
+
+flag = 1
+counter=0
+track_list=[[0 for col in range(2)] for row in range(500)]
+
+def Track(start,v0,sita):
+    global track_list
+    for t in range(500):
+        x=round(v0*math.cos(sita))
+        y=-round(v0*math.sin(sita)-0.3*t)
+        track_list[t]=[x,y]
+    print(track_list)
 
 def do_job():
-    canvas.coords(image,(0,0))    
+    global flag
+    global counter
+    global track_list
+    if flag == 1 :
+        print('init')
+        start=(200,200)   
+        Track(start,10,(math.pi)/12)
+        flag=0
 
+    if counter < 50 :
+        print(track_list[counter])
+        # canvas.coords(image,(track_list[counter]))
+        canvas.move(oval,track_list[counter][0],track_list[counter][1])
+        counter+=1
+
+def Task():
+    print('hello')
+    global timer
+    do_job()
+    timer=threading.Timer(1,Task)
+    timer.start()
+
+timer=threading.Timer(1,Task)
+timer.start()
 
 menubar = tk.Menu(window)
 filemenu = tk.Menu(menubar)
@@ -20,19 +57,18 @@ filemenu.add_separator()
 filemenu.add_command(label='Exit',command=window.quit)
 window.config(menu=menubar)
 
-canvas = tk.Canvas(window,bg='green',height=450,width=500)
+canvas = tk.Canvas(window,bg='green',height=550,width=800)
 img_file = tk.PhotoImage(file = 'images.png')
-image = canvas.create_image(0,0,anchor='nw',image=img_file)
+image = canvas.create_image(0,0,anchor='center',image=img_file)
 line = canvas.create_line(50,50,200,200,width= 3)
-oval = canvas.create_oval(200,200,300,300,fill='red')
+oval = canvas.create_oval(100,200,110,210,fill='red')
 canvas.pack()
 
 def moveit():
     canvas.move(image,5,5)
 
-b = tk.Button(window,text='move',command=moveit)
+b = tk.Button(window,text='move',command=do_job)
 b.pack()
 
-
 window.mainloop()
-
+timer.cancel()
